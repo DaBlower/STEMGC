701062d2498da551e03b54ac677fbebe6e52d000
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-06-12 16:04:37 +1000

    knight direction
    - add original knight sprites (camelot)
    - now the knight faces the current direction it is travelling in

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,24 +5,29 @@ extends CharacterBody2D
 @export var JUMP_VELOCITY = -300.0
 
 
-@onready var _animatedsprite = $AnimatedSprite2D
+@onready var animatedsprite = $AnimatedSprite2D
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction > 0: # going right
+		animatedsprite.flip_h = false # face right
+	elif direction < 0: # going left
+		animatedsprite.flip_h = true # face left
+	# this is an elif, because if no buttons are pressed, the character just stays facing in the current direction
+	# Applies the movement
 	if direction:
 		velocity.x = direction * SPEED
-		_animatedsprite.play("running")
+		animatedsprite.play("running")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		_animatedsprite.play("idle")
+		animatedsprite.play("idle")
 
 	move_and_slide()
