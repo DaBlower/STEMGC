53237ef4b74711d36a67c9624b92751c1f4919aa
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-06-12 17:00:16 +1000

    implement coyote time
    - implement coyote time (at 0.15)

--- scripts/player.gd
+++ scripts/player.gd
@@ -4,17 +4,19 @@ extends CharacterBody2D
 @export var SPEED = 85.0
 @export var JUMP_VELOCITY = -300.0
 
-
 @onready var animatedsprite = $AnimatedSprite2D
+@onready var coyote_timer: Timer = $CoyoteTimer
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and (is_on_floor || !coyote_timer.is_stopped()):
 		velocity.y = JUMP_VELOCITY
-
+	var was_on_floor = is_on_floor()
+	
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	if direction > 0: # going right because direction can be -1, 0 or 1
@@ -39,3 +41,5 @@ func _physics_process(delta: float) -> void:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
 	move_and_slide()
+	if was_on_floor && !is_on_floor():
+		coyote_timer.start()
