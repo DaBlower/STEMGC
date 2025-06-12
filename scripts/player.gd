0a243ee113f3eb5441ce73a1f3840c76e8c002e2
DaBlower <113282214+DaBlower@users.noreply.github.com>
2025-06-12 16:13:32 +1000

    better animation handling
    Current Issues
    - Player stays in jump animation even when on floor

--- scripts/player.gd
+++ scripts/player.gd
@@ -17,17 +17,25 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
-	if direction > 0: # going right
+	if direction > 0: # going right because direction can be -1, 0 or 1
 		animatedsprite.flip_h = false # face right
 	elif direction < 0: # going left
 		animatedsprite.flip_h = true # face left
 	# this is an elif, because if no buttons are pressed, the character just stays facing in the current direction
+	
+	if is_on_floor():
+		if direction == 0:
+			animatedsprite.play("idle")
+		else:
+			animatedsprite.play("run")
+	else:
+		animatedsprite.play("jump")
+
+	
 	# Applies the movement
 	if direction:
 		velocity.x = direction * SPEED
-		animatedsprite.play("running")
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		animatedsprite.play("idle")
 
 	move_and_slide()
